dc2e5961580d3942336c9f588bbc50ff6d24a51a
Gabriel <gab.hernandes@terra.com.br>
2020-08-30 19:22:51 -0300

    first commit

---
+++ app/src/main/java/com/example/gabriel/imagedemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.gabriel.imagedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    public void changeImage(View view){
+        Log.i("Info","Hello");
+        ImageView image = (ImageView) findViewById(R.id.cat);
+        image.setImageResource(R.drawable.cat2);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
